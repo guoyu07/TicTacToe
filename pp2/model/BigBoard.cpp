8daba252e205c80b3a8615a6a08988eb4bf26da6
Raghuvaran Chowda on HP <rchowda@g.clemson.edu>
2016-10-28 14:36:36 -0400

    Created BigBoard

---
+++ pp2/model/BigBoard.cpp
@@ -0,0 +1,29 @@
+//
+// Created by rchowda on 10/28/2016.
+//
+
+#include <iostream>
+#include "BigBoard.h"
+
+BigBoard::BigBoard(const Player &player1, const Player &player2) : player1(player1), player2(player2) {
+    for(int i=0; i<9; i++){
+        lBoard[i] = Board(player1,player2,i);
+    }
+    std::cout << lBoard.back().getId() <<","<< lBoard.size() << std::endl;
+}
+
+const std::array<Board, 9> &BigBoard::getLBoard() const {
+    return lBoard;
+}
+
+void BigBoard::setLBoard(const std::array<Board, 9> &lBoard) {
+    BigBoard::lBoard = lBoard;
+}
+
+
+int main(){
+    Player p1 = Player("RV",'x');
+    Player p2 = Player("SW",'y');
+
+    BigBoard bb = BigBoard(p1,p2);
+}
---
+++ pp2/model/BigBoard.h
@@ -0,0 +1,24 @@
+//
+// Created by rchowda on 10/28/2016.
+//
+
+#ifndef PP2_BIGBOARD_H
+#define PP2_BIGBOARD_H
+
+
+#include <array>
+#include "Board.h"
+
+class BigBoard {
+    Player player1, player2;
+    std::array<Board,9> lBoard;
+public:
+    BigBoard(const Player &player1, const Player &player2);
+
+    const std::array<Board, 9> &getLBoard() const;
+
+    void setLBoard(const std::array<Board, 9> &lBoard);
+};
+
+
+#endif //PP2_BIGBOARD_H
