dfbc3edb7d5673ce488dece4676eb10bd5795706
Raghuvaran Chowda on HP <rchowda@g.clemson.edu>
2016-10-30 00:20:15 -0400

    Re-implemented all function using rapidjson

--- pp2/model/PlayerDao.cpp
+++ pp2/model/PlayerDao.cpp
@@ -9,7 +9,7 @@
 #include "../lib/rapidjson/writer.h"
 #include "../lib/rapidjson/stringbuffer.h"
 
-bool PlayerDao::createNewPlayer(Player &player) {
+bool PlayerDao::createNewPlayer_old(Player &player) {
     if(isPresent(player.getName())) return false;
     if(!outFile.is_open()){outFile.open(fileName,std::ios_base::app);}
     outFile << player.getName() << std::endl;
@@ -18,7 +18,29 @@ bool PlayerDao::createNewPlayer(Player &player) {
     return true;
 }
 
-Player PlayerDao::getPlayer(std::string name) {
+bool PlayerDao::createNewPlayer(Player &player) {
+    if(isPresent(player.getName())) return false;
+    if(!outFile.is_open()){outFile.open(fileName,std::ios_base::app);}
+
+    rapidjson::Document json;
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    rapidjson::Document::AllocatorType& allocator = json.GetAllocator();
+    json.SetObject();
+    rapidjson::Value tmp_val;
+    tmp_val.SetString(player.getNameChar(),player.getName().length(),allocator);
+    json.AddMember("name", tmp_val,allocator);  //It's okay to have red line
+    tmp_val.SetString(player.getSymbolChar(), 1, allocator);
+    json.AddMember("symbol", tmp_val, allocator);  //It's okay to have red line
+    json.AddMember("win",0, allocator);json.AddMember("loss",0,allocator);json.AddMember("tie",0,allocator);
+    json.AddMember("uwin",0, allocator);json.AddMember("uloss",0,allocator);json.AddMember("utie",0,allocator);
+    json.Accept(writer);
+    outFile << buffer.GetString() << std::endl;
+    outFile.close();
+    return true;
+}
+
+Player PlayerDao::getPlayer_old(std::string name) {
     if(!inFile.is_open()){inFile.open(fileName);}
     std::string tmp_str;
     if(!inFile.eof()) std::getline(inFile,tmp_str);
@@ -33,8 +55,24 @@ Player PlayerDao::getPlayer(std::string name) {
     inFile.close();
     return Player();
 }
+//include player stats
+Player PlayerDao::getPlayer(std::string name) {
+    if(!inFile.is_open()){inFile.open(fileName);}
+    std::string tmp_str;
+    char* tmp_char;
+    rapidjson::Document json;
+    if(!inFile.eof()) {std::getline(inFile,tmp_str); tmp_char = &tmp_str[0]; json.Parse(tmp_char);}
+    while(!inFile.eof()) {
+        if (json.HasMember("name") && json["name"].GetString() == name && json.HasMember("symbol")){
+            return Player(json["name"].GetString(), json["symbol"].GetString());
+        }
+        std::getline(inFile,tmp_str); tmp_char = &tmp_str[0]; json.Parse(tmp_char);  //Fetch next
+    }
+    inFile.close();
+    return Player();
+}
 
-std::list<Player> PlayerDao::getAllPlayers() {
+std::list<Player> PlayerDao::getAllPlayers_old() {
     std::list<Player> playersList;
     if(!inFile.is_open()){inFile.open(fileName);}
     std::string tmp_name_str, tmp_sym_str;
@@ -51,8 +89,27 @@ std::list<Player> PlayerDao::getAllPlayers() {
     inFile.close();
     return playersList;
 }
+//include player stats
+std::list<Player> PlayerDao::getAllPlayers() {
+    std::list<Player> playersList;
+    if(!inFile.is_open()){inFile.open(fileName);}
+    std::string tmp_str, tmp_name_str, tmp_sym_str;
+    char* tmp_char;
+    rapidjson::Document json;
+    if(!inFile.eof()) {
+        std::getline(inFile,tmp_str); tmp_char = &tmp_str[0]; json.Parse(tmp_char);
+    }
+    while(!inFile.eof()){
+        if(json.HasMember("name") && json.HasMember("symbol")) {
+            playersList.push_back(Player(json["name"].GetString(), json["symbol"].GetString()));
+        }
+        std::getline(inFile,tmp_str); tmp_char = &tmp_str[0]; json.Parse(tmp_char);  //Fetch next line
+    }
+    inFile.close();
+    return playersList;
+}
 
-bool PlayerDao::isPresent(const std::string& name) {
+bool PlayerDao::isPresent_old(const std::string &name) {
     std::ofstream file;
     file.open("log.txt",std::ios_base::app);
     file << "case:" << name;
@@ -80,9 +137,39 @@ bool PlayerDao::isPresent(const std::string& name) {
     }
 }
 
-//int main(){
-//    PlayerDao playerDao;
-//    Player one("Raghuvaran",'R');
-//
-//    std::cout << playerDao.getPlayer("Raghuvaran").getSymbol();;
-//}
+bool PlayerDao::isPresent(const std::string &name) {
+    std::ofstream file;
+    file.open("log.txt",std::ios_base::app);
+    file << "case:" << name;
+    if(!inFile.is_open()) {inFile.open(fileName); file << "File Opened\n";}
+    if(inFile.good())
+    {   file << "It is good\n";
+        std::string tmp_str;
+        char* tmp_char;
+        rapidjson::Document json;
+        if(!inFile.eof()) {std::getline(inFile,tmp_str); tmp_char = &tmp_str[0]; json.Parse(tmp_char); file << "First Check has: " << tmp_str << "\n";}
+        while(!inFile.eof()){
+            if(json.HasMember("name") && json["name"].GetString() == name){
+                inFile.close();
+                file << "match"; file.close();
+                return true;
+            }
+            std::getline(inFile,tmp_str); tmp_char = &tmp_str[0]; json.Parse(tmp_char);
+        }
+        file << "not match"; file.close();
+        return false;
+
+    }
+    else{//TODO remove this line before submission
+        file << "not good"; file.close();
+       // std::cout << "Unable to find" + fileName + "\n";
+        return false;
+    }
+}
+
+int main(){
+    PlayerDao playerDao;
+    Player one("Raghuvaran",'R');
+
+    std::cout << playerDao.createNewPlayer(one);
+}
--- pp2/model/PlayerDao.h
+++ pp2/model/PlayerDao.h
@@ -15,12 +15,16 @@ class PlayerDao {
     std::ofstream outFile;
     std::ifstream inFile;
 public:
-    bool createNewPlayer(Player &);    //Create
+    bool createNewPlayer(Player &player);    //Create
+    bool createNewPlayer_old(Player &player);    //Obsolete
     Player getPlayer(std::string name); //Read
+    Player getPlayer_old(std::string name); //Obsolete
     std::list<Player> getAllPlayers();
-    //Update
+    std::list<Player> getAllPlayers_old();  //Obsolete
+    bool updatePlayerScore(Player& player, char game, int result); //Update
     //Delete
-    bool isPresent(const std::string& name);
+    bool isPresent_old(const std::string &name);
+    bool isPresent(const std::string &name);
 };
 
 
